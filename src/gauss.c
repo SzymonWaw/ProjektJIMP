5cfba3465068ee2ab027c026f4444320d16bb348
SzymonWaw <01206835@pw.edu.pl>
2025-12-15 23:42:05 +0100

    Implementacja eliminacji Gaussa + poprawka Makefile

--- src/gauss.c
+++ src/gauss.c
@@ -5,10 +5,31 @@
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
 
-		return 0;
+	int n = mat->r;
+    int k, i, j;
+
+    if (mat->r != mat->c || mat->r != b->r) {
+        return 1;
+    }
+
+    for (k = 0; k < n - 1; k++) {
+        
+
+        if (mat->data[k][k] == 0) {
+            return 1; 
+        }
+
+        for (i = k + 1; i < n; i++) {
+            
+            double factor = mat->data[i][k] / mat->data[k][k];
+            for (j = k; j < n; j++) {
+                mat->data[i][j] -= factor * mat->data[k][j];
+            }
+            b->data[i][0] -= factor * b->data[k][0];
+        }
+    }
+
+	return 0;
 }
 
