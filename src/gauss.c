d1738db00da26d4bd6bfe8017b0f7a30eb31ef32
moncior <olioziemblewski@gmail.com>
2025-12-19 17:32:31 +0100

    Zmiana gauss.c stud B

--- src/gauss.c
+++ src/gauss.c
@@ -1,35 +1,30 @@
 #include "gauss.h"
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
-int eliminate(Matrix *mat, Matrix *b){
+int eliminate(Matrix *mat, Matrix *b) {
+    if (mat->r != mat->c) return 1;
+    if (mat->r != b->r)   return 1;
 
-	int n = mat->r;
-    int k, i, j;
+    const int n = mat->r;
 
-    if (mat->r != mat->c || mat->r != b->r) {
-        return 1;
-    }
+    for (int k = 0; k < n - 1; k++) {
+        double pivot = mat->data[k][k];
 
-    for (k = 0; k < n - 1; k++) {
-        
-
-        if (mat->data[k][k] == 0) {
+        if (pivot == 0.0) {
             return 1; 
         }
 
-        for (i = k + 1; i < n; i++) {
-            
-            double factor = mat->data[i][k] / mat->data[k][k];
-            for (j = k; j < n; j++) {
-                mat->data[i][j] -= factor * mat->data[k][j];
+        for (int i = k + 1; i < n; i++) {
+            double *currentRow = mat->data[i];
+            double *pivotRow = mat->data[k];
+            double factor = currentRow[k] / pivot;
+
+            for (int j = k; j < n; j++) {
+                currentRow[j] -= factor * pivotRow[j];
             }
+
             b->data[i][0] -= factor * b->data[k][0];
         }
     }
 
-	return 0;
+    return 0;
 }
-
